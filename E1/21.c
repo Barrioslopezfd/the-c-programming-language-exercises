ca94c1b526dda20a940f0b58030f0cb4424d8b95
Franco D. Barrios Lopez <barrioslopezfd@gmail.com>
2025-11-02 06:09:11 -0300

    tmp

--- E1/21.c
+++ E1/21.c
@@ -17,25 +17,24 @@ int main()
 	char clean[MAX_SIZE];
 	while ((len = getln(line)) > 0)
 	{
-		clear(clean, line, len);
-		// printf("\"%s\"\n", clean);
-		printf("\"");
-		for (int i = 0; i < (sizeof(line) - sizeof(char)); i++)
-		{
-			if (line[i] == '\0') { printf("\\0"); }
-			else { putchar(line[i]); }
-			if ((i % 8) == 7) { printf(" | "); }
-		}
-		printf("\n");
 		printf(" < that should be printed: %d\n", debug_counter);
-		for (int i = 0; i < (sizeof(clean) - sizeof(char)); i++)
-		{
-			if (clean[i] == '\0') { printf("\\0"); }
-			else { putchar(clean[i]); }
-			if ((i % 8) == 7) { printf(" | "); }
-		}
-		printf("\"");
-		printf("\n");
+		printf("\"%s\"\n", line);
+		// printf("\"");
+		// for (int i = 0; i < (sizeof(line) - sizeof(char)); i++)
+		// {
+		// 	if (line[i] == '\0') { printf("\\0"); }
+		// 	else { putchar(line[i]); }
+		// 	if ((i % 8) == 7) { printf(" | "); }
+		// }
+		// printf("\n");
+		// for (int i = 0; i < (sizeof(clean) - sizeof(char)); i++)
+		// {
+		// 	if (clean[i] == '\0') { printf("\\0"); }
+		// 	else { putchar(clean[i]); }
+		// 	if ((i % 8) == 7) { printf(" | "); }
+		// }
+		// printf("\"");
+		// printf("\n");
 	}
 }
 
@@ -48,8 +47,7 @@ int getln(char line[])
 	int i = 0;
 	int z = 0;
 	int blanks=0;
-	int last = 0;
-	int mult = 0;
+	int last = -1;
 
 	while (i < MAX_SIZE - 1 && (c = getchar()) != EOF && c != '\n')
 	{
@@ -58,40 +56,28 @@ int getln(char line[])
 			blanks++;
 			if ( (i % TABSTOP) == 7 )
 			{
-				if ((blanks % TABSTOP) == 0) { mult++; }
-				printf(" i=%d - z=%d - blanks=%d - last=%d - mult=%d \n", i, z, blanks, last, mult);
-				z = i-(blanks+last);
-				printf("\n z=%d\n", z);
-				last = z;
-				line[last + 1]='>';
-				debug_counter++;
-				blanks = 0;
+				if (last == -1) 
+				{
+					last = z - blanks;
+					line[last] = '>';
+				}
+				else
+				{
+					line[last + 1]='>';
+					last++;
+					debug_counter++;
+					blanks = 0;
+				}
 			}
 		}
 		else
 		{
 			line[z]=c;
-			mult = 0;
+			last = z;
+			blanks = 0;
 		}
 		i++;
 		z++;
 	}
 	return z;
-}
-
-void clear(char to[],char from[], int len)
-{
-	memset(to, 0, MAX_SIZE);
-	int lastchar;
-	int z = 0;
-	for (int i = 0; i < len; i++)
-	{
-		if (from[i] != '\0') { 
-			to[z] = from[i];	
-			++z;
-			lastchar = z;
-		}
-	}
-	putchar(to[lastchar]);
-	to[lastchar] = '\0';
 }
